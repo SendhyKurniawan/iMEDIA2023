753a0ac793617817a7222cb5024127d1d000197f
Sendhy Kurniawan <53243422+SendhyKurniawan@users.noreply.github.com>
2023-08-08 08:33:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// get the div element
+const linktree1 = document.querySelector('#linktree1');
+
+// add a click event listener to the div
+linktree1.addEventListener('click', function() {
+  // specify the action to take when the div is clicked
+  window.open('https://drive.google.com/drive/folders/14wSkVFz6x_0E--yvVW1pKGMqqOE11d_J');
+
+});
