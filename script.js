e98bd33009947e1dc9f59193bec6b564d02e5312
Sendhy Kurniawan <53243422+SendhyKurniawan@users.noreply.github.com>
2023-08-24 08:42:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-// get the div element
-const linktree1 = document.querySelector('#linktree1');
-const linktree2 = document.querySelector('#linktree2');
-const linktree3 = document.querySelector('#linktree3');
-const linktree4 = document.querySelector('#linktree4');
-
-// add a click event listener to the div
-linktree1.addEventListener('click', function() {
-  // specify the action to take when the div is clicked
-  window.open('https://drive.google.com/drive/folders/14wSkVFz6x_0E--yvVW1pKGMqqOE11d_J');
-});
-// add a click event listener to the div
-linktree2.addEventListener('click', function() {
-  // specify the action to take when the div is clicked
-  window.open('https://drive.google.com/drive/folders/1HCU127AKXR77lkXbqnay-F6uJfUt_DBM');
-});
-// add a click event listener to the div
-linktree3.addEventListener('click', function() {
-  // specify the action to take when the div is clicked
-  window.open('https://drive.google.com/drive/folders/1xClBbeoF6AP93Q7eb_-h33OobjJGAPw1');
-});
-// add a click event listener to the div
-linktree4.addEventListener('click', function() {
-  // specify the action to take when the div is clicked
-  window.open('https://drive.google.com/drive/folders/1megra5-KVk5wJICRS37A4oQwIbRtyFUo');
-});
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  },
+);
